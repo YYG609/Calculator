a1a31c6841e92e6647347de1c137bdca68f71725
YYG609 <yyg864@gmail.com>
2023-11-29 17:28:02 +0900

    Lv.2

--- app/src/main/java/net/flow9/thisiskotlin/calculator/Calculator.kt
+++ app/src/main/java/net/flow9/thisiskotlin/calculator/Calculator.kt
@@ -1,6 +1,7 @@
 package net.flow9.thisiskotlin.calculator
 
 fun main() {
+    println("수식을 입력하세요")
     class Calculator{
         var num1 = readLine()!!.toDouble()
         var operator = readLine().toString()
@@ -23,6 +24,28 @@ fun main() {
                 }
                 "*" -> {
                     result = num1 * num2
+                    println("곱하기 결과 : ${result}")
+                }
+                else -> println("다시 시도해 주세요")
+            }
+            println("추가 연산 입력")
+            var moreOperation = readLine().toString()
+            var num3 = readLine()!!.toDouble()
+            when(moreOperation){
+                "+" -> {
+                    result += num3
+                    println("더하기 : ${result}")
+                }
+                "-" -> {
+                    result -= num3
+                    println("빼기 : ${result}")
+                }
+                "/" -> {
+                    result /= num3
+                    println("나누기 결과 : ${result}")
+                }
+                "*" -> {
+                    result *= num3
                     println("곱하기 결과 : ${result}")
                 }
                 else -> println("다시 시도해 주세요")
